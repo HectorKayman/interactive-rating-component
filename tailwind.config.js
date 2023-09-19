3669a66661ce8370ee8004758b3cdc57066cf355
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-09-19 18:45:16 +0530

    Add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-orange': 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+      },
+    },
   },
   plugins: [],
 };
